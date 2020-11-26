206f0b793c76c141680bc7f58a06b176ce461d06
festafolc <55447514+festafolc@users.noreply.github.com>
2020-11-26 01:55:02 +0000

    Add files via upload

--- Hollywood Manager/app.js
+++ Hollywood Manager/app.js
@@ -1,3 +1,29 @@
-function welcomePage (){
-    
+function register(){
+    function User(username, password){
+        this.username = username;
+        this.password = password;
+    }
+    let getUsername = document.getElementById("username").value;
+    let getPassword = document.getElementById("password").value;
+
+    newUser = new User(getUsername, getPassword);
+    add();
+}
+
+let dataBase = [];
+function add(){
+    dataBase.push(newUser);
+    console.log(dataBase);
+}
+
+function login(){
+    const username = document.getElementById("admin").value;
+    const password = document.getElementById("password").value;
+    if(username=="admin" && password=="admin"){
+        location.href="admin.html";
+    } else if (this.username && this.password){
+        location.href="index.html";
+    } else {
+        alert("Incorrect username or password");
+    }
 }
