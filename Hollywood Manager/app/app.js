6663aad5012f215f63fb2e26d3c6b3da189f4bd8
festafolc <55447514+festafolc@users.noreply.github.com>
2020-11-28 22:06:49 +0000

    Add files via upload

--- Hollywood Manager/app/app.js
+++ Hollywood Manager/app/app.js
@@ -1,9 +1,33 @@
-let favouritePeople = [];
+let app = new function() {
+    this.element = document.getElementById('pFavourites');
+    this.favouritePeople = [];
 
-function starPeople(event) {
-    let btn = document.getElementById(event.dataset.key)
-    console.log(btn.console.log(event));
-}
+    this.starPeople = function(event) {
+        let btn = event.getAttribute("data-key");
+        let fav = this.favouritePeople.findIndex(item => item == btn);
+        if(fav === -1){
+            this.favouritePeople.push(btn);
+            event.children[0].style = 'color: #FFDF00;';
+        } else {
+            this.favouritePeople.splice(fav, 1);
+            event.children[0].style = '';
+        }
+        this.showFavourites();
+    };
+
+    this.showFavourites = function (){
+        let data = '<h4>Your favourites are </h4>';
+        if (this.favouritePeople.length > 0){
+            for (i = 0; i < this.favouritePeople.length; i++){
+                data += this.favouritePeople[i] + ', ';
+            }
+        } else {
+            data = '';
+        }
+        return this.element.innerHTML = data.slice(0, -2); 
+    };
+};
+
 
 function register(){
     let registerUser = [];
